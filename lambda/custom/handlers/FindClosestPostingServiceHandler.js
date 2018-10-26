66f83085b0a4e4ccb60eba66aae1aac47ab2a02e
Johan Brunet <brunet.johan.23@gmail.com>
2018-10-26 18:06:39 +0200

    [not working] Always get 403

--- lambda/custom/handlers/FindClosestPostingServiceHandler.js
+++ lambda/custom/handlers/FindClosestPostingServiceHandler.js
@@ -13,13 +13,13 @@ const distanceInstance = axios.create({
 
 
 const instance = axios.create({
-    baseURL: 'https://api.laposte.fr/', 
+    baseURL: 'https://api.laposte.fr', 
     timeout: 5000,
     headers: { 'key': config.API_KEY }
 });
 
 const instanceAlexaApi = axios.create({
-    baseURL: 'https://api.amazonalexa.com/', 
+    baseURL: 'https://api.amazonalexa.com', 
     timeout: 5000
 })
 
@@ -35,42 +35,39 @@ const findClosestPostingServiceHandler = Alexa.CreateStateHandler(config.APP_STA
 
         instanceAlexaApi.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
 
+
         try {
             const userAddressRequest = await instanceAlexaApi.get(`/v1/devices/${deviceId}/settings/address`)
-            // const userAddress = userAddressRequest.data;
-            // if (userAddress.addressLine1 == null || userAddress.addressLine1 == "") {
-            //     ResponseHelper.sendResponse(alexa, alexa.t("NO_ADDRESS"));
-            // }
+            console.log("user address request: " + userAddressRequest)
+            const userAddress = userAddressRequest.data;
+            if (userAddress.addressLine1 == null || userAddress.addressLine1 == "") ResponseHelper.sendResponse(alexa, alexa.t("NO_ADDRESS"))
 
-            // let postAddresses;
-            // if(userAddress.postalCode) postAddresses = await instance.get(`/datanova/v1/pointscontact?q=${userAddress.postalCode}`) 
-            // else if(userAddress.city) postAddresses = await instance.get(`/datanova/v1/pointscontact?q=${userAddress.city}`) 
-            // else ResponseHelper.sendResponse(alexa, alexa.t("NO_ADDRESS"));
+            let postAddresses;
+            if(userAddress.postalCode) postAddresses = await instance.get(`/datanova/v1/pointscontact?q=${userAddress.postalCode}`) 
+            else if(userAddress.city) postAddresses = await instance.get(`/datanova/v1/pointscontact?q=${userAddress.city}`) 
+            else ResponseHelper.sendResponse(alexa, alexa.t("NO_ADDRESS"));
 
 
-            // const addressesForDistance = [`${userAddress.addressLine1}, ${userAddress.postalCode} ${userAddress.city}`]
-            // const addressArray = postAddresses.map( pa => {
-            //     addressesForDistance.push(`${pa.adresse}, ${pa.codePostal} ${pa.localite}`)
-            // });
+            const addressesForDistance = [`${userAddress.addressLine1}, ${userAddress.postalCode} ${userAddress.city}`]
+            const addressArray = postAddresses.map( pa => {
+                addressesForDistance.push(`${pa.adresse}, ${pa.codePostal} ${pa.localite}`)
+            });
 
-            // const distancesRequest = await distanceInstance.post({ "locations": addressArray })
+            const distancesRequest = await distanceInstance.post({ "locations": addressArray })
 
-            // if(distancesRequest.status != 200) ResponseHelper.sendResponse(alexa, alexa.t("DISTANCE_ERROR"));
+            if(distancesRequest.status != 200) ResponseHelper.sendResponse(alexa, alexa.t("DISTANCE_ERROR"));
 
-            // const distances = distancesRequest.data.distance
-            // const smallest = distances.sort()[1]
+            const distances = distancesRequest.data.distance
+            const smallest = distances.sort()[1]
 
-            // const nearest = addressArray.indexOf(distances.indexOf(smallest));
+            const nearest = addressArray.indexOf(distances.indexOf(smallest));
 
-            // let speechOutput = alexa.t("NEAREST_OFFICE_DISTANCE") + smallest + " kilomÃ¨tres, " + alexa.t("NEAREST_OFFICE_ADDRESS") + nearest;``
-            // ResponseHelper.sendResponse(alexa, speechOutput);
+            let speechOutput = alexa.t("NEAREST_OFFICE_DISTANCE") + smallest + " kilomètres, " + alexa.t("NEAREST_OFFICE_ADDRESS") + nearest;``
+            ResponseHelper.sendResponse(alexa, speechOutput);
         }
         catch(err) {
-            console.log("ERROR");
-            console.log(err);
-            console.log("error detail: " + err.data);
-            if(err.status == 403) ResponseHelper.sendResponse(alexa, alexa.t("NOTIFY_MISSING_PERMISSIONS"));
-            if(err.data.type == "FORBIDDEN") ResponseHelper.sendResponse(alexa, alexa.t("NOTIFY_MISSING_PERMISSIONS"));
+            console.log(err.response)
+            if(err.response.status == 403) ResponseHelper.sendResponse(alexa, alexa.t("NOTIFY_MISSING_PERMISSIONS"));
         }
  
     },
